1fa617324497cce226181a1f5cb0ed00f84a041e
Anzore <soldaev.anzor@gmail.com>
2024-10-18 15:38:46 +0200

    FIXED

--- PART2/exo4.php
+++ PART2/exo4.php
@@ -1,13 +1,17 @@
 <!--DONE-->
 
 
-<h1>Exercice 2</h1>
+<h1>Exercice 4</h1>
 
-<p>Soit le tableau suivant : <code>$capitales = 
-[France"=>"Paris","Allemagne"=>"Berlin","USA"=>"Washington","Italie"=>"Rome"];
-Réaliser un algorithme permettant d’afficher le tableau HTML suivant (notez que le nom du pays 
-s’affichera en majuscule et que le tableau est trié par ordre alphabétique du nom de pays) grâce à 
-une fonction personnalisée.</code> </p>
+<p>A partir de l’exercice 2, ajouter une colonne supplémentaire dans le tableau HTML qui contiendra 
+le lien hypertexte de la page Wikipédia de la capitale (le lien hypertexte devra s’ouvrir dans un 
+nouvel onglet et le tableau sera trié par ordre alphabétique de la capitale).
+On admet que l’URL de la page Wikipédia de la capitale adopte la forme :
+https://fr.wikipedia.org/wiki/
+Le tableau passé en argument sera le suivant :
+$capitales = ["France"=>"Paris","Allemagne"=>"Berlin",
+"USA"=>"Washington","Italie"=>"Rome","Espagne"=>"Madrid"];
+</p>
 
 <h2>Résultat</h2>
 

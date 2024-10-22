489081900ec5ce72bdc09325ab0f5802703a5aa3
Anzore <soldaev.anzor@gmail.com>
2024-10-22 09:09:49 +0200

    last exercices in php2

---
+++ PART2/exo13.php
@@ -0,0 +1,28 @@
+<!-- DONE WITH HELP OF CHAT GPT--> 
+ 
+<h1>Exercice 14</h1>
+
+<p>En utilisant les ressources de la page http://php.net/manual/fr/book.filter.php, vérifier si une 
+adresse e-mail a le bon format.
+
+<h2>Résultat</h2>
+
+<?php
+
+$email = "elan@elan-formation.fr"; // first problem was white space before the email, we can use trim() to eliminate any white spaces given, its a good practice when dealing with user input  
+
+$email = trim($email); // start using this 
+
+function validate($email){
+
+    $result = filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+    return $result;
+
+}
+
+if (validate($email)) { //technically i could put the if statement into the function to make it cleaner  ALSO the if statement doesnt need the ==true because the funtion has already verified if the given input is true or false so it basically looks liket his if(true) echo this else this!
+    echo "Your $email is a correct and valid!";
+    
+}else {
+    echo "Your $email is a incorrect and invalid!";
+}
--- PART2/exo3.php
+++ PART2/exo3.php
@@ -1,6 +1,6 @@
 <!--DONE-->
 
-<h1>Exercice 2</h1>
+<h1>Exercice 3</h1>
 
 <p>Afficher un lien hypertexte permettant d’accéder au site d’Elan Formation. Le lien devra s’ouvrir 
 dans un nouvel onglet (_blank)</p>

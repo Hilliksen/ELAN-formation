68cda3ffa365ec2a43162bb40875c337ce2580d4
Anzor <soldaev.anzor@gmail.com>
2024-11-21 11:02:06 +0100

    11:02

--- EXO 2 BASE/js/main.js
+++ EXO 2 BASE/js/main.js
@@ -1,9 +1,27 @@
 const cont = document.querySelector("#container");
 
 const box = document.createElement("div")
-box.classList.add("boxyellow")
+box.classList.add('box', 'yellow')
+const box2 = document.createElement("div")
+box2.classList.add('box', 'red')
+const box3 = document.createElement("div")
+box3.classList.add('box', 'blue')
+const box4 = document.createElement("div")
+box4.classList.add('box', 'green')
 
+cont.appendChild(box)
+cont.appendChild(box2)
+cont.appendChild(box3)
+cont.appendChild(box4)
 
-const newBox = box.nodeClode();
-console.log(newbox)
-cont.appendChild(newBox)
+let boxes = [box, box2, box3, box4];
+
+boxes.forEach(getAdd);
+
+function getAdd() {
+    box.addEventListener('click', ()=>{
+        window.getComputedStyle
+    })
+}
+
+console.log(getAdd)
